560bcd74d41a909787aa0c38efc57d396dfb6b72
collisionbox <21080012mk@edu.fca.ac.jp>
2022-11-25 11:32:10 +0900

    初期設定

---
+++ 3Dgame/main.cpp
@@ -0,0 +1,15 @@
+#include "DxLib.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	ChangeWindowMode(true);//ウィンドウモードに設定
+	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
+	{
+		return -1;			// エラーが起きたら直ちに終了
+	}
+
+
+
+
+	return 0;
+}
